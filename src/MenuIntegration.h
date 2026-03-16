4023823e58a36af4d651ff1ca7cbf402af2d64fe
Horf <pascal_schade@gmx.de>
2026-03-16 18:14:39 +0100

    - added SKSE MEnu Framework tooltips for the different options

--- src/MenuIntegration.h
+++ src/MenuIntegration.h
@@ -5,6 +5,20 @@
 
 namespace MenuIntegration
 {
+    inline void HelpMarker(const char* desc)
+    {
+        ImGuiMCP::SameLine();
+        ImGuiMCP::TextDisabled("(?)");
+        if (ImGuiMCP::IsItemHovered(ImGuiMCP::ImGuiHoveredFlags_DelayNormal))
+        {
+            ImGuiMCP::BeginTooltip();
+            ImGuiMCP::PushTextWrapPos(ImGuiMCP::GetFontSize() * 35.0f);
+            ImGuiMCP::TextUnformatted(desc);
+            ImGuiMCP::PopTextWrapPos();
+            ImGuiMCP::EndTooltip();
+        }
+    }
+
     inline void __stdcall RenderMenu()
     {
         bool changed = false;
@@ -18,36 +32,46 @@ namespace MenuIntegration
             Settings::fValueThresholdForLoot = threshold;
             changed = true;
         }
+        HelpMarker("Items with a gold value equal to or higher than this threshold will always be lootable.");
 
         ImGuiMCP::Spacing();
         ImGuiMCP::SeparatorText("Armor & Shields");
         if (ImGuiMCPComponents::ToggleButton("Hide Armor", &Settings::bUnlootableArmor)) changed = true;
+        HelpMarker("If disabled you can define bodyslot options (head, chest, arms and legs) seperately.");
 
         if (!Settings::bUnlootableArmor) {
             ImGuiMCP::Indent(15.0f);
             if (ImGuiMCPComponents::ToggleButton("Hide Head", &Settings::bUnlootableArmorHead)) changed = true;
+            HelpMarker("Includes Head, Hair and Circlets.");
             if (ImGuiMCPComponents::ToggleButton("Hide Chest", &Settings::bUnlootableArmorChest)) changed = true;
+            HelpMarker("Includes Body, Chest and Back.");
             if (ImGuiMCPComponents::ToggleButton("Hide Arms", &Settings::bUnlootableArmorArms)) changed = true;
+            HelpMarker("Includes Hands, Arms, Forearms and Shoulder.");
             if (ImGuiMCPComponents::ToggleButton("Hide Legs", &Settings::bUnlootableArmorLegs)) changed = true;
+            HelpMarker("Includes Feet, Leg, Calves and Pelvis.");
             ImGuiMCP::Unindent(15.0f);
         }
 
         if (ImGuiMCPComponents::ToggleButton("Hide Shields", &Settings::bUnlootableArmorShield)) changed = true;
         if (ImGuiMCPComponents::ToggleButton("Only Hide Worn Armor", &Settings::bArmorWornOnly)) changed = true;
+        HelpMarker("If enabled, only hides the armor/shield types the NPC is currently wearing.");
 
         ImGuiMCP::Spacing();
         ImGuiMCP::SeparatorText("Clothing");
         if (ImGuiMCPComponents::ToggleButton("Hide Clothing", &Settings::bUnlootableClothing)) changed = true;
         if (ImGuiMCPComponents::ToggleButton("Only Hide Worn Clothing", &Settings::bClothingWornOnly)) changed = true;
+        HelpMarker("If enabled, only hides the clothing types the NPC is currently wearing.");
 
         ImGuiMCP::Spacing();
         ImGuiMCP::SeparatorText("Weapons");
         if (ImGuiMCPComponents::ToggleButton("Hide Weapons", &Settings::bUnlootableWeapons)) changed = true;
         if (ImGuiMCPComponents::ToggleButton("Only Hide Worn Weapons", &Settings::bWeaponsWornOnly)) changed = true;
+        HelpMarker("If enabled, only hides the weapon types the NPC is currently wearing.");
 
         ImGuiMCP::Spacing();
         ImGuiMCP::SeparatorText("Pickpocket");
         if (ImGuiMCPComponents::ToggleButton("Apply to Pickpocketing", &Settings::bIncludePickpocket)) changed = true;
+        HelpMarker("If enabled, settings apply also while pickpocketing NPCs.");
 
         if (changed) {
             Settings::Save();
